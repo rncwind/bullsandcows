ff39c0f044ee14936f1343a39eac0dc1432f623b
rncwind <rncwnd@airmail.cc>
2017-12-04 14:33:56 +0000

    rewrite

--- gm1.cpp
+++ gm1.cpp
@@ -1,59 +1,51 @@
 #include <iostream>
-#include <string>
+#include<string>
 #include <vector>
 #include <array>
 #include <algorithm>
 
-int getbulls(std::array<bool,9> code, std::array<bool,9>guess, int& bulls);
-int getcows(std::array<bool,9> sortedcode, std::array<bool,9> guess, int bulls, int& cows);
+int getbulls(const std::array<bool,4>& code, const std::array<bool,4>& guess, int& bulls);
+int getcows(const std::array<bool,4>& sortedcode, const std::array<bool,4>& guess, const int bulls, int& cows);
 
-//lambda to print a container, saves on boilerplate
 auto const printcontainer = [](auto container){for(auto& i : container) std::cout << i;
 std::cout << '\n';};
 
 auto sortcode = [](auto container)
 {std::stable_sort(container.begin(),container.end()); return container;};
 
+auto strtoboolarr = [](auto& container, std::string input)
+{int it = 0; for (auto& i: input){container.at(it) = (i == '1'); ++it;}};
 
 int gm1loop()
 {
-    std::cout << "Gamemode 1 Selected\n";
-    std::array<bool,9> code {0,0,1,1,0,0,0,0,0};
-    std::array<bool,9> guess;
+    std::array<bool,4> code {1,0,0,0};
+    std::array<bool,4> guess;
 
-    std::array<bool,9> sortedcode;
+    std::array<bool,4> sortedcode;
     sortedcode = sortcode(code);
-
+    
     std::string input;
-    while(std::cin >> input)
+    while (std::cin >> input)
     {
         auto bulls = 0;
         auto cows = 0;
-        //clear guess and read in values
         guess.empty();
-        int it = 0;
-        for (auto& i : input){              //hack to convert str to arr of bool
-            guess.at(it) = (i == '1');
-            ++it;
-        }
-        //print guess
-        std::cout << "your guess is\n";
+        strtoboolarr(guess,input);
+        std::cout << "Your guess is: ";
         printcontainer(guess);
-
         if (guess == code){
-            std::cout << "You gussed correctly!" << std::endl;
-            return 0;                                           //indicate success
+            std::cout << "You guessed correctly" << std::endl;
+            return 0;
         }
         else{
-            std::cout << "Bulls: " << getbulls(code,guess, bulls);
-            std::cout << "\nCows: " << getcows(sortedcode, guess, bulls, cows) << '\n';
+            std::cout << "Bulls: " << getbulls(code,guess,bulls) << '\n';
+            std::cout << "\nCows: " << getcows(sortedcode,guess,bulls,cows) << '\n';
         }
     }
-    std::cout << std::endl;
     return 0;
 }
 
-int getbulls(std::array<bool,9> code, std::array<bool,9>guess, int& bulls)
+int getbulls(const std::array<bool,4>& code, const std::array<bool,4>& guess, int& bulls)
 {
     auto cit = code.begin();
     auto git = guess.begin();
@@ -66,18 +58,18 @@ int getbulls(std::array<bool,9> code, std::array<bool,9>guess, int& bulls)
     return bulls;
 }
 
-int getcows(std::array<bool,9> sortedcode, std::array<bool,9> guess, int bulls, int& cows)
-{
-    std::stable_sort(guess.begin(),guess.end());
-        auto tempcows = 0;
-    auto cit = sortedcode.begin();
-    auto git = guess.begin();
-    while (git != guess.end()){
-        if (*git == *cit)
-            ++tempcows;
-        ++git;
-        ++cit;
+
+int getcows(const std::array<bool,4>& sortedcode, const std::array<bool,4>& guess, const int bulls, int& cows){
+    auto scit = sortedcode.begin();
+    auto guessit = guess.begin();
+    while (guessit != guess.end()){
+        if(*guessit == *scit)
+            ++cows;
+        ++scit;
+        ++guessit;
     }
-    cows = (tempcows - bulls);
-    return cows;
+    if (cows > bulls)
+        return (cows-bulls);
+    else
+        return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
-#include <vector>
 
 int gm1loop();
 int gm2loop();
 
 int main()
 {
-    std::cout << "enter (1) for Gamemode 1\nenter (2) for Gamemode2\n>";
-    int mode = 0;
+    std::cout << "Enter (1) for Gamemode 1\nEnter (2) for Gamemode 2\n>";
+    auto mode = 0;
     std::cin >> mode;
     switch(mode){
-    case 1:
-        gm1loop(); break;
-    case 2:
-        gm2loop();
-        break;
+        case 1:
+            gm1loop(); break;
+        case 2:
+            gm2loop(); break;
     }
     return 0;
 }
