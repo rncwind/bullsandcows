efc5bbe321cfadb688e368d7cf9045f8a529e41a
rncwind <rncwind@airmail.cc>
2017-12-05 14:12:54 +0000

    Brute-force soloution to gm2

--- gm2.cpp
+++ gm2.cpp
@@ -4,12 +4,28 @@
 #include <bitset>
 
 std::array<bool,4> indexToCode(int i);
+int getBulls(const std::array<bool,4>& code, const std::array<bool,4>& guess, int& bulls);
+int getCows(const std::array<bool,4>& sortedcode, const std::array<bool,4>& guess, const int bulls, int& cows);
 
 auto const printContainer = [](auto container){for(auto& i : container) std::cout << i;
 std::cout << '\n';};
 
 auto strToBoolArr = [](auto& container, std::string input)
 {int it = 0; for (auto& i: input){container.at(it) = (i == '1'); ++it;}};
+
+void bruteforce(std::array<bool,4> code)
+{
+    for (int i = 0; i < 15; ++i)
+    {
+        if (indexToCode(i) == code)
+        {
+            std::cout << "Your code is: ";
+            printContainer(indexToCode(i));
+            std::cout << "found after " << i << " Iterations";
+            break;
+        }
+    }
+}
 
 int gm2loop()
 {
@@ -18,13 +34,9 @@ int gm2loop()
     std::cout << "Input your code\n>";
     std::cin >> input;
     strToBoolArr(code,input);
-    printContainer(indexToCode(2));
-    printContainer(indexToCode(4));
-    printContainer(indexToCode(8)); 
-    printContainer(indexToCode(15));
-    return 9000;
+    bruteforce(code);
+    return 0;
 }
-
 
 std::array<bool,4> indexToCode(int i){
     std::string codestr = std::bitset<4>(i).to_string();
