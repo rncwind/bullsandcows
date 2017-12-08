210b1f7e8ec6e475aa7c44caf1426c2bf2b7069e
rncwind <rncwind@airmail.cc>
2017-12-08 20:31:34 +0000

    helps to actually add the file you change

--- gm1.cpp
+++ gm1.cpp
@@ -1,32 +1,39 @@
+#include <algorithm>
+#include <array>
 #include <iostream>
+#include <random>
 #include <string>
 #include <vector>
-#include <array>
-#include <algorithm>
-#include <random>
-#include <functional>
 
+//function prototypes
 int getBulls(const std::array<bool,9>& code, const std::array<bool,9>& guess, int& bulls);
 int getCows(const std::array<bool,9>& sortedcode, const std::array<bool,9>& guess, const int bulls, int& cows);
 
-auto const printContainer = [](auto container){for(auto& i : container) std::cout << i;
-std::cout << '\n';};
+//lambdas for reccurant boilerplate
+//names are self doccumenting, code on one line for style
+auto const printContainer = [](const auto& container)
+{for(auto& i : container) std::cout << i; std::cout << '\n';};
 
+//takes a copy of the code and sorts it returning the sorted copy. used later to find cows
 auto sortCode = [](auto container)
 {std::stable_sort(container.begin(),container.end()); return container;};
 
-auto strToBoolArr = [](auto& container, std::string input)
+auto strToBoolArr = [](auto& container, const std::string& input)
 {int it = 0; for (auto& i: input){container.at(it) = (i == '1'); ++it;}};
 
 void generateCode(std::array<bool,9>& code)
 {
+    //get entropy from platform entropy source (eg /dev/random for *nix)
+    //create a mersenne twister from it and use that in a [0,1] distribution
+    //to generate randoms for the code passed in as refference
     std::random_device rdev;
+    std::mt19937 twister(rdev());
     std::uniform_int_distribution<int> distrib(0,1);
     for (int it = 0; it < 9; ++it)
-           code.at(it) = distrib(rdev);
+           code.at(it) = distrib(twister);
 }
 
-
+//main game loop and control logic
 int gm1loop()
 {
     std::array<bool,9> code;
@@ -56,6 +63,8 @@ int gm1loop()
     return 0;
 }
 
+//iterate through the contents of the guess and the code comparing values symmetrically
+//if the same incriment bulls. bulls passed as refference for genericism's sake
 int getBulls(const std::array<bool,9>& code, const std::array<bool,9>& guess, int& bulls)
 {
     auto cit = code.begin();
@@ -69,7 +78,10 @@ int getBulls(const std::array<bool,9>& code, const std::array<bool,9>& guess, in
     return bulls;
 }
 
-int getCows(const std::array<bool,9>& sortedcode, const std::array<bool,9>& guess, const int bulls, int& cows){
+//simmilar to getCows, however operating on a sorted copy of the code to break.
+//if there are more matches found in this version, subtracts the amount of bulls from the cows
+//to provide an accurate figure. cows passed in for genericism, bulls passed in as a copy
+int getCows(const std::array<bool,9>& sortedcode, const std::array<bool,9>& guess, const int& bulls, int& cows){
     auto scit = sortedcode.begin();
     auto guessit = guess.begin();
     while (guessit != guess.end()){
